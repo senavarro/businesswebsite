855c161da8d2f6e8925450cd5c2565349cdf5321
Sergi Navarro <sergi.navarro@gmail.com>
2019-07-04 16:36:59 +0200

    Added repository, models and views created, frontend added

---
+++ businessweb/services/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Service(models.Model):
+	title = models.CharField(max_length=200)
+	subtitle = models.CharField(max_length=200)
+	content = models.TextField()
+	image = models.ImageField(upload_to="services")
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		verbose_name = "service"
+		verbose_name_plural = "services"
+		ordering = ["-created"]
+
+	def __str__(self):
+		return self.titles
